6b7939881633c14f4e2853053dc50c02a107e7e1
HuDi <hdgbdn92@gmail.com>
2021-06-27 16:53:59 +0800

    03

---
+++ 01_Accustoming_Yourself_to_Cpp/03_bitwise_const_mutable_function_reuse.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+// bitwise const: a const member function means that it don't change any datamember(non-static)
+// logic const: a const member function may change some data member, but it can't be found from outside
+// The compiler support bitwise const: a const member shouldn't change even one bit of datamember
+// but some time we need to change some member, so here come keyword: mutable
+
+class CTextBlock{
+public:
+    CTextBlock(const char* c){
+        pText = (char*)malloc(strlen(c) + 1); 
+        strcpy(pText, c);
+        cout << "created " << pText << endl;
+    }
+    char& operator[](size_t position) { return pText[position]; }
+    char& operator[](size_t position) const{ return pText[position]; }
+    size_t length() const;
+private:
+    char* pText;
+    // add some data to cache length
+    mutable size_t textLength;
+    mutable bool lengthIsValid;
+};
+
+size_t CTextBlock::length() const
+{
+    if(!lengthIsValid){
+        textLength = strlen(pText);         // if don't add mutable, we can't change data member in a const memberfunction
+        lengthIsValid = true;               // expression must be a modifiable lvalue
+    }
+    return textLength;
+}
+
+class TextBlock{
+public:
+    TextBlock(const char *c)
+    {
+        pText = (char *)malloc(strlen(c) + 1);
+        strcpy(pText, c);
+        cout << "created " << pText << endl;
+    }
+    const char& operator[](size_t position) const{
+        // some common operations in both const and non const
+        return pText[position];
+    }
+    char& operator[](size_t position){
+        return
+            const_cast<char&>(
+                static_cast<const TextBlock&>(*this)[position]
+            );
+    }
+private:
+    char *pText;
+};
+
+int main()
+{
+    const CTextBlock b("hi nihao");
+    char* get = &b[0];
+    cout << *get << endl;        // "h"
+    *get = 'n';
+    cout << b[0] << endl;       // although passed compiler, but it can change data member by reference!!
+
+    // function reuse
+    // if const and non const version of member function works same, we don't need two sets of codes
+    // we use the non const version to call the const version
+    // but how to call const version [] in non const version?
+    // static_cast<const TextBlock&>(*this) cast *this to const TextBlock&(why reference?)
+    // then the const version of [] operator will be called 
+    // static_cast<const TextBlock&>(*this)[position] -- calling const version []
+    // and ther result is const, so use and only can use const_cast to remove constness
+
+    // why use non const version to call cosnt version
+    // because const can be removed, but can't be added
+    // if call non const version in const, the non const version may break the constness
+}
---
+++ 01_Accustoming_Yourself_to_Cpp/03_use_const_whenever_possible.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Rational
+{
+public:
+    Rational(int v) : val(v) {}
+    int val;
+};
+
+const Rational operator*(const Rational& lhs, const Rational& rhs){
+    Rational ret(lhs.val * rhs.val);
+    return ret;
+}
+
+class TextBook{
+public:
+    TextBook(string s = "new book"): text(s) {}
+    const char& operator[](size_t position) const { cout << "using const version []" << endl; return text[position]; }
+    char& operator[](size_t position) { cout << "using non const version [] " << endl; return text[position]; }
+private:
+    string text;
+};
+
+int main()
+{
+    // const restrict compiler
+    // pointers is a little bit complex
+    // const on the left of '*', means the pointed object is constant
+    // const on the right of '*', means the pointer itself is constant
+    // on both sides, means the pointer and the object pointed to are both constant
+    char greeting[] = "Hello";
+    char* p1 = greeting;
+    const char* p2 = greeting;
+    char* const p3 = greeting;
+    const char* const p4 = greeting;
+    // also, when pointed object is constant, there are two types, both are correct
+    auto constPointed = [](const int* i)->int const *{
+        return new int(*i);
+    };
+    int const * val = new int(10);
+    const int* ret = constPointed(val);
+    cout << *val << endl;       // 10
+    cout << *ret << endl;       // 10
+
+    // STL iterator and pointer
+    {
+        // iterator is base on pointer, like a T*
+        // so the const behaviour is same
+        vector<int> vec(10, 1);
+        const vector<int>::iterator iter = vec.begin();     // the iterator itself is const
+        *iter = 20;                                         // correct
+        // ++iter;                                          // error: no operator "++" matches these operands -- operand types are: ++ const
+        vector<int>::const_iterator cIter = vec.begin();    // the object iterator pointed to is constant
+        // cIter = 2;                                       // expression must be a modifiable lvalue
+        ++cIter;
+    }
+
+    // return a constant ojbect
+    {
+        Rational a(2), b(3);
+        Rational c(4);
+        // avoid meanless assignment
+        // (a * b) = c;            // no operator "=" matches these operands -- operand types are: const Rational = Rational
+    }
+
+    // const member function
+    {
+        // ! const member can be modified!
+        // only if defined a const member function
+        // const member function is change this pointer to const pointer, to a pointer point to constant object
+        TextBook tb("Hello");
+        cout << tb[0] << endl;
+
+        const TextBook ctb("World");
+        cout << ctb[0] << endl;
+
+        tb[0] = 'x';        // write to a non const char, correct
+        cout << tb[0] << endl;
+        // ctb[0] = 'x';    // expression must be a modifiable lvalue
+
+        // if operator returns a non referenced char:
+        // tb[0] = 'x' will be error, because try to modify a temp value
+    }
+    return 1;
+}
