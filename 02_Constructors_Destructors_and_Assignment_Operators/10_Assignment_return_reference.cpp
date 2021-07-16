d6e03ae40462c16f065d7c8ad0994d50845c1f04
hdgbdn <hdgbdn92@gmail.com>
2021-07-16 16:05:38 +0800

    10

---
+++ 02_Constructors_Destructors_and_Assignment_Operators/10_Assignment_return_reference.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	// if you want:
+	int x, y, z;
+	x = y = z = 42;
+	// then you should let the operator= return a reference to *this
+	return 0;
+}
