15393eb0fe55751cebe32e91e14158245ff67232
hdgbdn <hdgbdn92@gmail.com>
2021-07-17 10:02:23 +0800

    06 add notes

--- 02_Constructors_Destructors_and_Assignment_Operators/06_Disallow_synthesized_functions.cpp
+++ 02_Constructors_Destructors_and_Assignment_Operators/06_Disallow_synthesized_functions.cpp
@@ -12,8 +12,8 @@ using namespace std;
 // first declar a uncopyable class
 class Uncopyable {
 protected:
-    Uncopyable() {}
-    ~Uncopyable() {}
+    Uncopyable() { cout << "Uncopyable constructor" << endl; }
+    ~Uncopyable() { cout << "Uncopyable destructor" << endl; }
 private:
     Uncopyable(const Uncopyable&);
     Uncopyable& operator=(const Uncopyable&);
@@ -44,7 +44,15 @@ int main()
 {
     HomeForSale myHome;
     // HomeForSale hisHome(myHome);        // error: a deleted function
+	// HomeForSale inherent from Uncopyable, the base class's constructor's constructor is private, so what will happen when we create a HomeForSale?
+	// will the synthesized default constructor call the base's constructor?
+	// the constructor will be called, because the constructor and destructor is protected, the derived class can access it.
     NewHomeForSale myNewHome;
     // NewHomeForSale hisHome(myNewHome);  // error: a deleted function
 	// because the base class: NewUncopyable has deleted copy construction and assignment
+
+	// Summary:
+	// to prevent copy operations, we can set copy constructor and copy assignment as private, or in new standard, use '= delete'
+	// and we can create a base class which copy operation is inaccessible(private or deleted), then derived class can't access it's base class's
+	// copy operation, so it prevent compiler to synthesize copy operations. (but if you defined by yourself, then it is still copyable)
 }
