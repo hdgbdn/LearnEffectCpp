69a0cf729e2fffd66f39b89805f9da2c11f54be7
hdgbdn <hdgbdn92@gmail.com>
2021-07-22 22:10:43 +0800

    19

---
+++ 04_Designs_and_Declarations/19_Treat_class_design_as_type_design.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	// design your class as type
+
+	// how to construct and destruct
+
+	// difference between copy construction and copy assignment
+
+	// what happened when pass by value
+
+	// what is the legal value
+
+	// restricted by inheritance graph?
+
+	// need conversation?
+
+	// operator or member function
+
+	// delete what standard function
+
+	// undeclared interface
+
+	// how generic
+
+	// really need a new class?
+	return 0;
+}
